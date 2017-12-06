e06b01a54e2a11a95a0a9b2940adb98a2a9c3c08
RasEza <rasmus.appel@gmail.com>
2017-12-06 14:47:08 +0100

    more renaming

--- testDecorator/testDecorator/PizzaTopping.cs
+++ testDecorator/testDecorator/PizzaTopping.cs
@@ -10,30 +10,30 @@ namespace testDecorator
     public class PizzaTopping : Pizza
     {
         //This is what enables it to be decorated
-        private Pizza pizza;
+        private Pizza _pizza;
 
         //A Pizza topping can take any pizza type class in the constructor, meaning you can cascade constructions
         public PizzaTopping(Pizza p)
         {
-            pizza = p;
+            _pizza = p;
         }
 
         //Calling display on its child pizza object
         public override void Display()
         {
-            pizza.Display();
+            _pizza.Display();
         }
 
         //Calling cost on its child pizza object and returning the result
         public override double Cost()
         {
-            return pizza.Cost();
+            return _pizza.Cost();
         }
 
         //Calling tasteIndex on its child pizza object and returning the result
         public override double TasteIndex()
         {
-            return pizza.TasteIndex();
+            return _pizza.TasteIndex();
         }
     }
 }
